044d1fafe6eb84ef571a58c5a420821b50f3a043
asa <asa@192.168.86.41>
2019-01-10 23:38:48 -0500

    Began work on TableView and ObjectTableView for displaying tabular data to the user. TableView works, exccept for adding columns.

--- WheelsWithinWheels/src/wheelswithinwheels/WheelsWithinWheels.java
+++ WheelsWithinWheels/src/wheelswithinwheels/WheelsWithinWheels.java
@@ -5,6 +5,9 @@
  */
 package wheelswithinwheels;
 
+import wheelswithinwheels.commandui.TableView;
+import wheelswithinwheels.commandui.TableViewWidthOverflowException;
+
 /**
  *
  * @author asa
@@ -15,7 +18,21 @@ public class WheelsWithinWheels {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            TableView tv = new TableView(3);
+            String[] row = {"foo","bar","baz"};
+            tv.addRow(row);
+            String[] row2 = {"f","bs","sdkkdjksjkjsd"};
+            tv.addRow(row2);
+            tv.addColumns(2);
+            String[] row3 = {"fred","","","","joe"};
+            tv.addRow(row3);
+            System.out.println(tv);
+            
+            System.out.println("done");
+        } catch (TableViewWidthOverflowException e){
+            System.out.println(e);
+        }
     }
     
 }
---
+++ WheelsWithinWheels/src/wheelswithinwheels/commandui/TableViewWidthOverflowException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wheelswithinwheels.commandui;
+
+/**
+ *
+ * @author asa
+ */
+public class TableViewWidthOverflowException extends Exception {
+
+    public TableViewWidthOverflowException() {
+        super("Tried to add row to table, but row was too large.");
+    }
+    
+}
